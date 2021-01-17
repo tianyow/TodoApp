1336f8058ce322f1e35b747ac4240df074f59a1b
tianyow <tianyow@gmail.com>
2021-01-17 23:01:21 +0800

    Initial Commit

---
+++ Todo App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo App
+//
+//  Created by TIAN MBPR on 2021/1/17.
+//  Copyright © 2021 PSC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
