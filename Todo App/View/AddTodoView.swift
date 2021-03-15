d9746dfe3918e0d829c94ad1d826221cc59ded47
tianyow <tianyow@gmail.com>
2021-03-16 00:07:18 +0800

    first commit

---
+++ Todo App/View/AddTodoView.swift
@@ -0,0 +1,21 @@
+//
+//  AddTodoView.swift
+//  Todo App
+//
+//  Created by TIAN MBPR on 2021/1/17.
+//  Copyright © 2021 PSC. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddTodoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddTodoView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddTodoView()
+    }
+}
