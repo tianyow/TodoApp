e80c0c35f3c7d868aff693b5d07106347596b4a6
tianyow <tianyow@gmail.com>
2021-03-16 23:56:01 +0800

    commit 2

--- Todo App/View/AddTodoView.swift
+++ Todo App/View/AddTodoView.swift
@@ -9,10 +9,53 @@
 import SwiftUI
 
 struct AddTodoView: View {
+  // MARK: - PROPERTIES
+  
+  @Environment(\.presentationMode) var presentationMode
+  
+  @State private var name: String = ""
+  @State private var priority: String = "Normal"
+  
+  let priorities = ["High", "Normal", "Low"]
+  
+  // MARK: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      NavigationView {
+        VStack {
+          Form {
+            // MARK: - TODO NAME
+            TextField("Todo", text: $name)
+            // MARK: - TODO PRIORITY
+            Picker("Priority", selection: $priority) {
+              ForEach(priorities, id: \.self) {
+                Text($0)
+              }
+            }
+          .pickerStyle(SegmentedPickerStyle())
+            
+          // MARK: - SAVE BUTTON
+            Button(action: {
+              print("Save a new todo item.")
+            }) {
+              Text("Save")
+            } //: SAVE BUTTON
+          } //: FORM
+          
+          Spacer()
+        } //: VSTACK
+          .navigationBarTitle("New Todo", displayMode: .inline)
+        .navigationBarItems(trailing:
+          Button(action: {
+            self.presentationMode.wrappedValue.dismiss()
+          }) {
+            Image(systemName: "xmark")
+          }
+        )
+      } //: NAVIGATION
     }
 }
+
+// MARK: - PREVIEW
 
 struct AddTodoView_Previews: PreviewProvider {
     static var previews: some View {
--- Todo App/View/ContentView.swift
+++ Todo App/View/ContentView.swift
@@ -9,11 +9,33 @@
 import SwiftUI
 
 struct ContentView: View {
+  // MARK: - PROPERTIES
+  
+  @State private var showingAddTodoView: Bool = false
+  
+  // MARK: - BODY
     var body: some View {
-        Text("Hello, World!")
+      NavigationView {
+        List(0 ..< 15) { item in
+          Text("Hello, World!")
+        } //: LIST
+          .navigationBarTitle("Todo", displayMode: .inline)
+          .navigationBarItems(trailing:
+            Button(action: {
+              self.showingAddTodoView.toggle()
+            }) {
+              Image(systemName: "plus")
+            } //: ADD BUTTON
+              .sheet(isPresented: $showingAddTodoView) {
+                AddTodoView()
+            }
+        
+        )
+      } //: NAVIGATION
     }
 }
 
+// MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
